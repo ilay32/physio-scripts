dab2fb54d6b37a47b04be78d7b8e7f3ead888b0c
ilay32 <silayster@gmail.com>
2018-07-10 14:54:50 +0300

    GaitForce Treadmill: Use Fz to Correct Heel Strike Identification

--- GaitForceEvents.m
+++ GaitForceEvents.m
@@ -18,6 +18,82 @@ classdef GaitForceEvents
         right_hs
     end
     methods (Static)
+        function hs = improve_on_cop(bycopx,rightend,fz)
+            % improve hs identification with force data.
+            % within the ith stretch, find maximal fz slope
+            % then scan from cop identification forward up to the max slope index,
+            % until the downslope point preceeding a
+            % the longest positive slope is reached.
+            start = bycopx - floor(GaitForceEvents.scanwindow/2);
+            fz = fz(start:rightend);
+            dfz = diff(fz);
+            % rule of thumb, search from the point found by cop up to the
+            % point where fz is steepest
+            [maxslope,mi] = max(dfz);
+            if mi == 1
+                hs = 'maximal slope is at left end';
+                return;
+            end
+            fz = fz(1:mi+1);
+            % separations between down slopes are the lengths of the up slopes
+            increase_lengths = diff(find(dfz(1:mi) < 0));
+            if isempty(increase_lengths)
+                origminlength = mi;
+            else
+                origminlength = max(increase_lengths);
+            end
+            function slopestart = longsteep(minslope,minlength)
+                % returns the index preceeding a minlength-long sequence
+                % in fz, where fz only rises and the overall steepness is
+                % high. first the steepness is relaxed, then the length
+                absminslope = 0.7*maxslope;
+                slopestart = 1;
+                if minlength == 1 && minslope <= absminslope
+                    % scanning for the long slope was complete with no
+                    % results. so find minimal fz
+                    [~,minval_index] = min(fz);
+                    dsv = length(fz) - minval_index;
+                    
+                    % if it's far from the right end, take the last point
+                    % of really flat fz
+                    if  dsv > length(fz)/2
+                        slopestart = find(diff(fz) < 0.2*maxslope,1,'last');
+                        % fz is never flat in this range -- take 2 before
+                        % steepest
+                        if isempty(slopestart)
+                            slopestart = length(fz) - 2;
+                        end
+                    % if it's pretty close, take the low point 
+                    else
+                        slopestart = minval_index;
+                    end
+                    return;
+                end
+                while slopestart < length(fz) - minlength
+                    stretch = fz(slopestart:slopestart+minlength);
+                    if isempty(stretch)
+                        return;
+                    end
+                    if all(diff(stretch) > 0) && (stretch(end) - stretch(1))/minlength >= minslope
+                        return;
+                    end
+                    slopestart = slopestart + 1;
+                end
+                if minslope > absminslope
+                    minslope = minslope*0.95;
+                elseif minslope <= absminslope
+                    minslope = maxslope;
+                    minlength = minlength - 1;
+                end
+                slopestart = longsteep(minslope,minlength);
+            end
+            long_increase_starts = longsteep(maxslope,origminlength);
+            if start + long_increase_starts > bycopx
+                hs = start + long_increase_starts;
+            else
+                hs = 'was of no use';
+            end
+        end
         function ind = scanbackward(dat,idxs,curind,thresh)
             if nargin == 3
                 thresh = GaitForceEvents.slope_thresh;
@@ -31,7 +107,7 @@ classdef GaitForceEvents
             while cur > leftlim
                 avg = mean(dat(cur  - sc:cur));
                 if avg < thresh
-                    ind = cur - floor(sc/2);
+                    ind = cur;
                     return
                 end
                 cur = cur - sc;
@@ -39,7 +115,7 @@ classdef GaitForceEvents
             % if not found, scan again with larger threshold
             % fprintf('not found between %d and %d\n',idxs(curind),leftlim);
             %ind = round((idxs(curind) - idxs(curind - 1))/2);
-            ind = GaitForceEvents.scanbackward(dat,idxs,curind,thresh*1.5);
+            ind = GaitForceEvents.scanbackward(dat,idxs,curind,thresh*1.2);
         end
         
         function d = squish(data,window)
@@ -75,51 +151,41 @@ classdef GaitForceEvents
             % ends of plataeus are detected as heel strikes.
             % it will also plot it for visual inspection
             copx = self.forces(:,3);
-            % if the treadmill was run in revese, just flip the copx
-            if self.reversed
-                copx = -1*copx;
-            end
             scopx = movmean(copx,GaitForceEvents.smoothwindow);
             deriv = diff(scopx) * self.datarate;
             smoothed_deriv = movmean(deriv,GaitForceEvents.smoothwindow);
             % minima of the copx derivative mean that weight was moving
-            % sharply from right so the points where this negative weight shift started
+            % sharply from right to left so the points where this negative weight shift started
             % correspond to left heel strikes. for right ones -- the
             % oppsite.
-            allhs = cell(1,2);
-            for side = [-1,1] % left right 
-                [~,idx] = findpeaks(smoothed_deriv*side,'MinPeakDistance',...
+            allhs = struct;
+            for side = {'left','right'} 
+                slopesign = 1;
+                if strcmp(side{:},'left')
+                    slopesign = -1;
+                end
+                
+                % if the treadmill was run in revese, the left foot down
+                % will be the copx positive peaks
+                if self.reversed
+                    slopesign = -1*slopesign;
+                end
+                [~,idx] = findpeaks(smoothed_deriv*slopesign,'MinPeakDistance',...
                     self.deriv_peak_dist,'MinPeakHeight',2*GaitForceEvents.slope_thresh);
                 heel_strikes = nan*ones(length(idx),1);
                 for i = 2:length(idx)
-                    bycopx = GaitForceEvents.scanbackward(smoothed_deriv*side,idx,i);
+                    bycopx = GaitForceEvents.scanbackward(smoothed_deriv*slopesign,idx,i);
                     heel_strikes(i) = bycopx;
-                    % now try to correct it with fz data:
-                    % within the ith stretch, find steepest fz, then scan
-                    % back until a a point where fz decresses
-                    start = bycopx - GaitForceEvents.scanwindow;
-                    fz = self.forces(start:idx(i),2);
-                    dfz = diff(fz);
-                    [~,mi] = max(dfz);
-                    if mi > 1
-                        disp('trying to use fz');
-                        better = find(dfz(1:mi) < 0,1,'last');
-                        if better
-                            if start + better > bycopx
-                                disp('prefer fz');
-                                heel_strikes(i) = start + better;
-%                                 if better > length(fz)/2
-%                                     disp('but moderating')
-%                                     heel_strikes(i) = start + floor(better/2);
-%                                 end
-                            else
-                                disp('was of no use');
-                            end
-                            
-                        end
-                    else
-                        disp('max fz slope is not between...')
+                    byfz = GaitForceEvents.improve_on_cop(bycopx,idx(i),self.forces(:,2));
+                    if isnumeric(byfz) && byfz > bycopx && byfz < idx(i)
+                        %disp('prefer fz');
+                        heel_strikes(i) = byfz;
                     end
+                   % elseif isnumeric(byfz)
+                   %     fprintf('range limits: [%d,%d] forces result: %d\n',bycopx,idx(i),byfz);
+                   % else
+                   %     disp(byfz);
+                   % end
                 end
                 % unite really close points that can arise due to the
                 % scanbackward recursion
@@ -127,30 +193,44 @@ classdef GaitForceEvents
                 % eliminate nan, that can arise due to failed
                 % identification
                 heel_strikes = heel_strikes(~isnan(heel_strikes));
-                allhs{1,ceil((side+2)/2)} = [heel_strikes,scopx(heel_strikes)]; % -1 --> 1, 1 --> 2
+                allhs.(side{:}) = [heel_strikes,scopx(heel_strikes)];
             end
-            % show what was found 
+            % show what was found
+            figure;
             c = plot(scopx);
             hold on;
             dotts = cell(0,2);
-            for side = 1:size(allhs,2)
-                hs = allhs{1,side};               
+            for side = 1:2
+                sidename = 'left';
+                if side == 2
+                    sidename = 'right';
+                    pstyle = 'g*';
+                else
+                    pstyle = 'r*';
+                end
+                hs = allhs.(sidename);
                 for i = 1:length(hs)
-                    if side == 1
-                        pstyle = 'r*';
-                    else
-                        pstyle = 'g*';
-                    end
-                    dotts{i,side} = plot(hs(i,1),hs(i,2),pstyle);
+                    dotts{i,side} = plot(hs(i),scopx(hs(i)),pstyle);
                 end
             end
             lh(1) = c;
             lh(2) = dotts{:,1};
             lh(3) = dotts{:,2};
+            self.add_fz([allhs.left;allhs.right]);
             legend(lh,{'copx','left hs','right hs'});
             hold off;
-            self.left_hs = allhs{1,1};
-            self.right_hs = allhs{1,2};
+            self.left_hs = allhs.left;
+            self.right_hs = allhs.right;
+        end
+        
+        function add_fz(self,hss)
+            fz = self.forces(:,2);
+            plot((movmean(fz,GaitForceEvents.smoothwindow/2)/mean(fz))*max(self.forces(:,3) + 0.05))
+            ylm = get(gca,'Ylim');
+            for i=1:length(hss)
+                linex = hss(i);
+                line([linex,linex],ylm,'color',[0.5,0.5,0.5]);
+            end
         end
         
         function s = get_strikes(self,side)
@@ -200,59 +280,90 @@ classdef GaitForceEvents
             wds = abs(self.step_diffs(side,2));
         end
         
-        function check_against_forcegait(self)
+        function check_against_gaitforce(self)
             % read left heel strikes times from "List of COP points.txt"
             pointsfile = dir([self.datafolder '/*COP*.txt']);
+            gaitforce = struct('list',[],'numlist',0,'cycles',[],'numcycles',0);
+            rawcopx = self.forces(:,3);
             if isempty(pointsfile)
-                disp('Sorry, couldn''t find ForceGaits list of COP points');
-                return
-             end
-            fgpoints = importdata(fullfile(pointsfile.folder,pointsfile.name));
-            if all(strcmp(fgpoints.textdata(:,1),''))
-                fgpoints.textdata = fgpoints.textdata(:,2:end);
+                disp('could not find gaitforce''s list of COP points');
+            else
+                fgpoints = importdata(fullfile(pointsfile.folder,pointsfile.name));
+                % sometimes the right column is blank
+                if all(strcmp(fgpoints.textdata(:,1),''))
+                    fgpoints.textdata = fgpoints.textdata(:,2:end);
+                end
+                gaitforce.list = fgpoints.data(strcmp(fgpoints.textdata(:,2),'HSL'),3);
+                gaitforce.numlist = length(gaitforce.list);
             end
-            fglhs = fgpoints.data(strcmp(fgpoints.textdata(:,2),'HSL'),3);
             
             % read the same from "GaitCycleParameters"
-            cyc = importdata(fullfile(self.datafolder,GaitForceEvents.cycles_filename));
-            fglhs2 = cell2mat(cellfun(@str2num,cyc.textdata(:,1),'UniformOutput',false));
-
-            % compare between forcegait and itself
-            if length(fglhs2) ~= length(fglhs)
-                disp('Force Gait left heel strikes are not the same number');
-                fprintf('cycles file: %d, list of cop points: %d\n',length(fglhs2),length(fglhs));
-                l = min(length(fglhs),length(fglhs2));
-                fglhs = fglhs(1:l);
-                fglhs2 = fglhs2(1:l);
+            cycfile = fullfile(self.datafolder,GaitForceEvents.cycles_filename);
+            if exist(cycfile,'file')
+                cyc = importdata(cycfile);
+                gaitforce.cycles = cell2mat(cellfun(@str2num,cyc.textdata(:,1),'UniformOutput',false));
+                gaitforce.numcycles = length(gaitforce.cycles);
+            else
+                disp('could not find GaitForce''s cycles file');
             end
             
-            fgdif = abs(fglhs - fglhs2);
-            fgmdif = mean(fgdif);
-            fgmaxdif = max(fgdif);
-            if any(fgdif > 1/self.datarate) || any(fgmaxdif > self.deriv_peak_dist/2/self.datarate)
-                fprintf('Force Gait left heel strikes dont agree.');
-                fprintf('Mean difference: %fs (%.3f readings), max difference: %.3f\n',...
-                    fgmdif,fgmdif*self.datarate,fgmaxdif);
+            if ~(gaitforce.numlist || gaitforce.numcycles)
+                return;
             end
+            % compare between gaitforce and itself and print stuff to
+            % console if needed
+            if gaitforce.numcycles && gaitforce.numlist
+                if length(gaitforce.cycles) ~= length(gaitforce.list)
+                    disp('Force Gait left heel strikes are not the same number');
+                    fprintf('cycles file: %d, list of cop points: %d\n',gaitforce.numcycles,gaitforce.numlist);
+                    l = min(gaitforce.numcycles,gaitforce.numlist);
+                    gaitforce.list = gaitforce.list(1:l);
+                    gaitforce.cycles = gaitforce.cycles(1:l);
+                end
+                fgdif = abs(gaitforce.list - gaitforce.cycles);
+                fgmdif = mean(fgdif);
+                fgmaxdif = max(fgdif);
+                if any(fgdif > 1/self.datarate) || any(fgmaxdif > self.deriv_peak_dist/2/self.datarate)
+                    fprintf('Force Gait left heel strikes dont agree.');
+                    fprintf('Mean difference: %fs (%.3f readings), max difference: %.3f\n',...
+                        fgmdif,fgmdif*self.datarate,fgmaxdif);
+                end
+            end
+            
+            % use the list of cop points as reference if exists
+            if gaitforce.numcycles && ~gaitforce.numlist
+                ref = gaitforce.cycles;
+            else
+                ref = gaitforce.list;
+            end    
+            
             figure;
-                c = plot(self.forces(:,3));
+                c = plot(rawcopx);
                 hold on;
-                ours = self.left_hs;
+                ours = self.left_hs(:,1);
                 ourd = {};
                 for i = 1:length(ours)
-                    ourd{i} = plot(ours(i,1),ours(i,2),'r*');
+                    ourd{i} = plot(ours(i),rawcopx(ours(i)),'r*');
                 end
-                % assuming the cycles data might be wrong
-                fglhs = round((fglhs2 - self.forces(1,1))*self.datarate); % from seconds to readings
+                % force gait left heel strikes in readings
+                fglhs = round((ref - self.forces(1,1))*self.datarate);
                 theird = {};
                 for i = 1:length(fglhs)
-                    theird{i} = plot(fglhs(i),self.forces(fglhs(i),3),'g*');
+                    theird{i} = plot(fglhs(i),rawcopx(fglhs(i)),'g*');
                 end
                 lh(1) = c;
                 lh(2) = ourd{:};
                 lh(3) = theird{:};
+                self.add_fz([fglhs;ours]);
                 legend(lh,{'copx','our lhs','gaitforce lhs'});
                 hold off;
+            % some very basic statistics
+            l = min(length(ours),length(fglhs));
+            ours = ours(1:l,1);
+            theirs = fglhs(1:l);
+            discrep = ours - theirs;
+            fprintf('maximal difference: %d\nmean difference: %d\nstd: %.4f\n',...
+                max(abs(discrep)),mean(discrep),std(discrep));
         end
         function quick_export(self)
            % just dump all L/R steptimes,durations and widths to one csv
@@ -268,6 +379,8 @@ classdef GaitForceEvents
                    exp.([f{:} '_' side{:}]) = col;
                end
            end
+           exp.left_heel_strikes = self.left_hs(:,1)/self.datarate;
+           exp.right_heel_strikes = self.right_hs(:,1)/self.datarate;
            fnms = fieldnames(exp);
            t = nan*ones(longest,length(fnms));
            for f = 1:size(t,2)
--- gaitforcequick.m
+++ gaitforcequick.m
@@ -2,3 +2,8 @@ folder = uigetdir();
 ge = GaitForceEvents(folder);
 ge = ge.find_heel_strikes();
 ge.quick_export();
+comp = input('compare with GaitForce? [y/n] ','s');
+if strcmp(comp,'y')
+    ge.check_against_gaitforce();
+end
+
